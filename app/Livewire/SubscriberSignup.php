f4c369ab7ed8358caaea6390fa428b0fe9ed6677
Marcel Stadelmann <m@marceli.to>
2025-04-15 18:32:22 +0200

    wip

--- app/Livewire/SubscriberSignup.php
+++ app/Livewire/SubscriberSignup.php
@@ -15,14 +15,16 @@ class SubscriberSignup extends Component
   {
     $this->validate();
 
-    Subscriber::create([
-      'email' => $this->email,
-    ]);
+    if (!Subscriber::where('email', $this->email)->exists()) {
+      
+      Subscriber::create([
+        'email' => $this->email,
+      ]);
 
-    Notification::route('mail', $this->email)->notify(new SubscriberNotification([
-      'email' => $this->email,
-    ]));
-
+      Notification::route('mail', $this->email)->notify(new SubscriberNotification([
+        'email' => $this->email,
+      ]));
+    }
     session()->flash('status', 'You have been subscribed to the newsletter!');
     $this->reset('email');
   }
